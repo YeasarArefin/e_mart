3792747d5547052d30e33d96f0e9d995af4378e9
YeasarArefin <yeasararefin007@gmail.com>
2024-07-17 14:36:49 +0600

    change : url changed

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,34 @@ const nextConfig = {
     images: {
         domains: ['adminapi.applegadgetsbd.com']
     },
-    /* async rewrites() {
+    async headers() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'http://localhost:3000/api/:path*',
+                // Routes this applies to
+                source: "/api/(.*)",
+                // Headers
+                headers: [
+                    // Allow for specific domains to have access or * for all
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://exclusive-mart.vercel.app/",
+                        // DOES NOT WORK
+                        // value: process.env.ALLOWED_ORIGIN,
+                    },
+                    // Allows for specific methods accepted
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET, POST, PUT, DELETE, OPTIONS",
+                    },
+                    // Allows for specific headers accepted (These are a few standard ones)
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
+                    },
+                ],
             },
         ];
-    }, */
+    },
 };
 
 export default nextConfig;
