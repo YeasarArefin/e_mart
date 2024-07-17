53acb89bff90a141d07e9e7231a04efeb7e3c43a
YeasarArefin <yeasararefin007@gmail.com>
2024-07-17 14:20:36 +0600

    change : working like before

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
     images: {
         domains: ['adminapi.applegadgetsbd.com']
-    }
+    },
+    /* async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3000/api/:path*',
+            },
+        ];
+    }, */
 };
 
 export default nextConfig;
